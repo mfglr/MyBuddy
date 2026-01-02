b0e61cbf9ac2f2d4f1d3c93fc3086a7cb2040bf0
mfglr <mfgglr@outlook.com>
2026-01-03 02:58:39 +0300

    feat(post): updated the use case "CreatePostMedia"

---
+++ PostService/PostService.Application/Exceptions/UnauthorizedOperationException.cs
@@ -0,0 +1,5 @@
+﻿namespace PostService.Application.Exceptions
+{
+    public class UnauthorizedOperationException()
+        : Exception("Forbidden. Insufficient permissions to perform this operation.");
+}
--- PostService/PostService.Application/UseCases/CreatePostMedia/CreatePostMediaConsumer.cs
+++ PostService/PostService.Application/UseCases/CreatePostMedia/CreatePostMediaConsumer.cs
@@ -1,22 +1,27 @@
 ﻿using MassTransit;
+using PostService.Application.Exceptions;
 using PostService.Application.UseCases.CreatePost;
 using PostService.Domain;
 using PostService.Domain.Exceptions;
 
 namespace PostService.Application.UseCases.CreatePostMedia
 {
-    public class CreatePostMediaConsumer(IBlobService blobService, IPostRepository postRepository) : IConsumer<CreatePostMediaRequest>
+    public class CreatePostMediaConsumer(IBlobService blobService, IPostRepository postRepository, IIdentityService identityService) : IConsumer<CreatePostMediaRequest>
     {
         private readonly IBlobService _blobService = blobService;
         private readonly IPostRepository _postRepository = postRepository;
+        private readonly IIdentityService _identityService = identityService;
 
         public async Task Consume(ConsumeContext<CreatePostMediaRequest> context)
         {
             var types = CreatePostMediaHelpers.GetMediaTypes(context.Message.Media);
 
-            var post = 
+            var post =
                 await _postRepository.GetByIdAsync(context.Message.Id, context.CancellationToken) ??
                 throw new PostNotFoundException();
+
+            if (post.UserId != _identityService.UserId)
+                throw new UnauthorizedOperationException();
 
             var blobNames = await _blobService.UploadAsync(Post.MediaContainerName, context.Message.Media, context.CancellationToken);
 
@@ -24,7 +29,7 @@ namespace PostService.Application.UseCases.CreatePostMedia
 
             try
             {
-                post.AddMedia(media,context.Message.Offset);
+                post.AddMedia(media, context.Message.Offset);
                 await _postRepository.UpdateAsync(post, context.CancellationToken);
                 await context.RespondAsync(CreatePostMediaMapper.ToCreatePostMediaResponse(post.Id, media));
             }
