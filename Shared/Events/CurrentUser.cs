4e5be9bbe7d5ed8766c68699bddb20ff4106a0c4
mfglr <mfgglr@outlook.com>
2026-03-20 01:19:59 +0300

    feat: Create CurrentUser.

---
+++ Shared/Events/CurrentUser.cs
@@ -0,0 +1,17 @@
+﻿using Media.Models;
+
+namespace Shared.Events
+{
+    public record CurrentUserMedia(
+        ModerationResult? ModerationResult,
+        IEnumerable<Thumbnail> Thumbnails
+    );
+
+    public record CurrentUser(
+        Guid Id,
+        int Version,
+        string UserName,
+        string? Name,
+        CurrentUserMedia? Media
+    );
+}
